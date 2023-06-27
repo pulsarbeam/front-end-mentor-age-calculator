261eec80d54557349c2388525085f2bbdafecf95
Rene Groothuis <renegrooth@gmail.com>
2023-06-27 20:52:59 +1200

    Fixed font and styling

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100 font-poppins">
-        <div className="bg-white rounded-xl p-8">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-200 font-poppins">
+        <div className="bg-white rounded-xl rounded-br-[200px] p-8">
           <form
             onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
               e.preventDefault()
@@ -47,28 +47,28 @@ export default function Home() {
             className=" font-bold"
           >
             <div className="flex gap-6 ">
-              <div className="flex flex-col gap-1">
-                <label className="text-md">DAY</label>
+              <div className="flex flex-col gap-2">
+                <label className="text-xs tracking-widest text-gray-400">DAY</label>
                 <input
-                  className="w-24 py-4 px-2 border border-gray-400 rounded-lg text-xl "
+                  className="w-32 py-2 px-4 border border-gray-400 rounded-lg text-2xl "
                   name="day"
                   id="day"
                   placeholder="DD"
                 ></input>
               </div>
-              <div className="flex flex-col gap-1">
-                <label className="text-md">MONTH</label>
+              <div className="flex flex-col gap-2">
+                <label className="text-xs tracking-widest text-gray-400">MONTH</label>
                 <input
-                  className="w-24 py-4 px-2 border border-gray-400 rounded-lg text-xl "
+                  className="w-32 py-2 px-4 border border-gray-400 rounded-lg text-2xl "
                   name="month"
                   id="year"
                   placeholder="MM"
                 ></input>
               </div>
-              <div className="flex flex-col gap-1">
-                <label className="text-md">YEAR</label>
+              <div className="flex flex-col gap-2">
+                <label className="text-xs tracking-widest text-gray-400">YEAR</label>
                 <input
-                  className="w-24 py-4 px-2 border border-gray-400 rounded-lg text-xl "
+                  className="w-32 py-2 px-4 border border-gray-400 rounded-lg text-2xl "
                   name="year"
                   id="year"
                   placeholder="YYYY"
@@ -76,7 +76,7 @@ export default function Home() {
               </div>
             </div>
             <div className="relative">
-              <hr className="border-b-gray-400 my-10 w-[30rem]" />
+              <hr className="border-b-gray-400 my-10 w-[35rem]" />
               <button className=" absolute right-0 -top-10 h-20 w-20 rounded-full bg-purple-600">
                 <img
                   src="/icon-arrow.svg"
@@ -86,24 +86,24 @@ export default function Home() {
               </button>
             </div>
           </form>
-          <div className="flex flex-col gap-6 text-xl font-extrabold ">
+          <div className="flex flex-col gap-4 text-xl bold ">
             <div className="flex gap-4 items-center">
-              <span className="text-purple-500 text-5xl">
+              <span className="text-purple-500 text-7xl">
                 {elapsed.years ? elapsed.years : '- -'}
               </span>
-              <span className="font-extrabold text-7xl italic">years</span>
+              <span className="font-extrabold text-7xl ">years</span>
             </div>
             <div className="flex gap-4 items-center">
-              <span className="text-purple-500 text-5xl">
+              <span className="text-purple-500 text-7xl">
                 {elapsed.months ? elapsed.months : '- -'}
               </span>
-              <span className="text-7xl italic">months</span>
+              <span className="text-7xl ">months</span>
             </div>
             <div className="flex gap-4 items-center">
-              <span className="text-purple-500 text-5xl">
+              <span className="text-purple-500 text-7xl">
                 {elapsed.days ? elapsed.days : '- -'}
               </span>
-              <span className="text-7xl italic ">days</span>
+              <span className="text-7xl  ">days</span>
             </div>
           </div>
         </div>
